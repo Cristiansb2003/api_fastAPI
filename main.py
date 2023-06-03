2d8df03dd0dcf69e4209a720dc4b80b4d26c4a80
cristian <cristian192003@outlook.es>
2023-06-03 13:42:14 -0600

    Fin de la actividad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from database import database as connection
+from database import User
+from database import UserReview
+from database import Movie
 
 app = FastAPI(title='Proyecto para reseñar peliculas',
               description='En este proyecto seremos capaces de reseñar peliculas.',
@@ -9,6 +12,7 @@ def startup():
     if connection.is_closed():
         connection.connect()
         print('Conexion exitosa')
+    connection.create_tables([User, Movie, UserReview])
 
 @app.on_event('shutdown')
 def shutdown():
