e51c0f91a62352333f4bc22d66e9d5f30c49f20a
cristian <cristian192003@outlook.es>
2023-06-05 15:02:51 -0600

    fin de la practica

--- schemas.py
+++ schemas.py
@@ -33,12 +33,21 @@ class UserResponseModel(ResponseModel):
     id: int
     username:str
 
+
+#-------------------Review
 class ReviewRequestModel(BaseModel):
     user_id: int
     movie_id: int
     review: str
     score: int
 
+    @validator('score')
+    def score_validator(cls,score):
+        if score < 1 or score > 5:
+            raise ValueError('El rango para score es de 1 a 5')
+        
+        return score
+    
 class ReviewResponseModel(ResponseModel):
     id:int
     movie_id: int
