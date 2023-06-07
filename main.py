6805d1328e9d91fdca1695698574f77848625043
cristian <cristian192003@outlook.es>
2023-06-07 17:33:39 -0600

    fin de la practica

--- main.py
+++ main.py
@@ -65,8 +65,10 @@ async def create_review(user_review: ReviewRequestModel):
     return user_review
 
 @app.get('/reviews', response_model=List[ReviewResponseModel])
-async def get_reviews():
-    reviews = UserReview.select()#SELECT * FROM user_reviews;
+async def get_reviews(page:int = 1, limit:int = 10):
+    #el primer argumento(page) hace referencia a la pagina actual
+    #el segundo argumento hace referencia a la capacitad maxima
+    reviews = UserReview.select().paginate(page,limit)#SELECT * FROM user_reviews;
 
     return [user_review for user_review in reviews]
 
