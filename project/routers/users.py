75dfdda49e5daaf1bb0fbdcdda508a5425937668
cristian <cristian192003@outlook.es>
2023-06-13 14:22:12 -0600

    Fin de la practica

--- project/routers/users.py
+++ project/routers/users.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from fastapi.security import HTTPBasicCredentials
+
 from fastapi import HTTPException
 from ..database import User
 from ..schemas import UserRequestModel
@@ -16,4 +18,16 @@ async def create_user(user:UserRequestModel):
         username = user.username,
         password = hash_password
     )
+    return user
+
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials):
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(404, 'User not found')
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(404, 'Password error')
+    
     return user
