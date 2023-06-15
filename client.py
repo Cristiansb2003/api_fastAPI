a8274b7ee0f2a48af817b90cf142ece4e17fac98
cristian <cristian192003@outlook.es>
2023-06-15 08:14:11 -0600

    Se puede enviar y recibir encabezados

--- client.py
+++ client.py
@@ -2,7 +2,14 @@ import requests
 
 URL = 'http://localhost:8000/api/v1/reviews'
 
-response = requests.get(URL)
+HEADERS = {'accept': 'application/json'}
+
+response = requests.get(URL, headers=HEADERS)
 
 if response.status_code == 200:
     print('Peticion realizada de forma exitosa!')
+    
+    if response.headers.get('content-type') == 'application/json':
+        reviews = response.json()
+        for review in reviews:
+            print(f"score: {review['score']}-{review['review']}")
